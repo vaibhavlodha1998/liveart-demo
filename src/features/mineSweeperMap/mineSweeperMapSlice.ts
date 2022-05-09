7bcf479678f861a75e350313a9ccc9df8af97c94
Vaibhav Lodha <vaibhavlodha1998@gmail.com>
2022-05-09 18:03:30 +0530

    Without Redux

--- src/features/mineSweeperMap/mineSweeperMapSlice.ts
+++ src/features/mineSweeperMap/mineSweeperMapSlice.ts
@@ -1,34 +1,34 @@
-import {
-    createSlice,
-    PayloadAction,
-  } from '@reduxjs/toolkit';
-  import type { RootState } from '../../store/store';
+// import {
+//     createSlice,
+//     PayloadAction,
+//   } from '@reduxjs/toolkit';
+//   import type { RootState } from '../../store/store';
 
-  export type MineSweeperMapState = {
-    value: Array<any>;
-  };
+//   export type MineSweeperMapState = {
+//     value: Array<any>;
+//   };
 
-  const initialState: MineSweeperMapState = {
-    value: [],
-  };
+//   const initialState: MineSweeperMapState = {
+//     value: [],
+//   };
 
 
-  export const mineSweeperMapSlice = createSlice({
-    name: 'mineSweeperMap',
-    initialState,
-  reducers: {
+//   export const mineSweeperMapSlice = createSlice({
+//     name: 'mineSweeperMap',
+//     initialState,
+//   reducers: {
 
-      updateMineSweeperMap: (state, action: PayloadAction<Array<any>>) => {
-        return [...state.value,...action.payload]
-      },
-    },
-  });
+//       updateMineSweeperMap: (state, action: PayloadAction<Array<any>>) => {
+//         return [...state.value,...action.payload]
+//       },
+//     },
+//   });
   
-  export const {
-    updateMineSweeperMap
-  } = mineSweeperMapSlice.actions;
+//   export const {
+//     updateMineSweeperMap
+//   } = mineSweeperMapSlice.actions;
 
   
-    export const selectMineSweeperMap = (state: RootState) => state.mineSweeperMap;
+//     export const selectMineSweeperMap = (state: RootState) => state.mineSweeperMap;
   
-  export default mineSweeperMapSlice.reducer;
+//   export default mineSweeperMapSlice.reducer;
